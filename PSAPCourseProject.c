b8029ab57f8ab03cc25cc55003f6e39287f7a617
SuyashBhavalkar@3 <suyashbhavalkar82@gmail.com>
2024-07-30 22:44:41 +0530

    committed change

--- PSAPCourseProject.c
+++ PSAPCourseProject.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
+#include<time.h>
 
-struct tranction_history // To store Tranction History
+struct tranction_history // To store Tranction Histor
 {
     int year;
     int month;
